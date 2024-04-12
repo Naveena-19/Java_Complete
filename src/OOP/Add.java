23ac01e29e8db187b8b224c4300ceba311b4c714
naveena-19 <naveenakumari1902@example.com>
2024-04-12 11:33:31 +0530

    addition of two numbers using class and object

--- src/OOP/Add.java
+++ src/OOP/Add.java
@@ -2,9 +2,22 @@ package OOP;
 
 public class Add {
     public static void main(String[] args) {
-        int num1=8;
-        int num2=9;
-        int result=num1+num2;
+//        int num1=8;
+//        int num2=9;
+//        int result=num1+num2;
+//        System.out.println(result);
+        Calculator calc = new Calculator();//object created using new keyword
+//        calc= reference variable
+
+       int result= calc.add(8,9);//using the object call the add method
         System.out.println(result);
+    }
+}
+class Calculator
+{
+    public int add(int num1,int num2)
+    {
+       int r=num1+num2;
+        return r;
     }
 }

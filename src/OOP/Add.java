413ab093462dde21b63eb73ec3763e4290870d08
naveena-19 <naveenakumari1902@example.com>
2024-04-12 10:16:16 +0530

    addition of two numbers

---
+++ src/OOP/Add.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class Add {
+    public static void main(String[] args) {
+        int num1=8;
+        int num2=9;
+        int result=num1+num2;
+        System.out.println(result);
+    }
+}

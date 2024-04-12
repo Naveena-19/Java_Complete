6a91c381e6e75694181c2650cf0df780d848ac98
naveena-19 <naveenakumari1902@example.com>
2024-04-12 18:37:24 +0530

    theory

--- src/OOP/Theory.java
+++ src/OOP/Theory.java
@@ -7,6 +7,11 @@ public class Theory
         System.out.println("class is blue print of objects\n" +
                 "objects are created by JVM\n" +
                 "objects have properties and behaviovrs\n" +
-                "the word new is used to create the object\n");
+                "the word new is used to create the object\n" +
+                "JVM<JRE<JDK\n" +
+                "the reference variables and local variables are stored in stack" +
+                " but the original object and instance variables [=-are stored in heap\n" +
+                "the reference variable have the address or link of the object stored in stack\n" +
+                "");
     }
 }

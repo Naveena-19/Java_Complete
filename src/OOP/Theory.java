da8f872bb0226c6acf1c095fcfb6649ccfe6eb04
naveena-19 <naveenakumari1902@example.com>
2024-04-12 09:51:58 +0530

    Object Oriented Programming

---
+++ src/OOP/Theory.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Theory
+{
+    public static void main(String[] args)
+    {
+        System.out.println("class is blue print of objects\n" +
+                "objects are created by JVM\n" +
+                "objects have properties and behaviovrs\n");
+    }
+}
